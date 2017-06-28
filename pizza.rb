760cc7aade0638dec17d9b5c87d80a0b0c80f0b8
brandon <bscarberry23@yahoo.com>
2017-06-28 10:26:48 -0400

    first commit

---
+++ pizza.rb
@@ -0,0 +1,26 @@
+def kind()
+	 print "What kind of pizza would you like?"
+	 kind = gets.chomp
+end
+
+def size()
+	print "What size do you want?"
+	size = gets.chomp
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+kind()
+size()
